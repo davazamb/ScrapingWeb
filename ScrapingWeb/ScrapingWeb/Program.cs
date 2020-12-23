6fd3f5212d2ae6bf51e311b8db8092f155e4fcb3
davaz <davazamb10@outlook.com>
2020-12-23 13:14:55 -0300

    Get Body Page

--- ScrapingWeb/ScrapingWeb/Program.cs
+++ ScrapingWeb/ScrapingWeb/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScrapingWeb
 {
@@ -10,6 +9,18 @@ namespace ScrapingWeb
     {
         static void Main(string[] args)
         {
+            List<string> lista = new List<string>();
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://github.com/davazamb/ScrapingWeb");
+
+            HtmlNode body = doc.DocumentNode.CssSelect("Body").First();
+            string htmBody = body.InnerHtml;
+
+            foreach (var item in doc.DocumentNode.CssSelect(".pr-3"))
+            {
+                var nodoAn = item.CssSelect("a").First();
+                lista.Add(nodoAn.InnerHtml);
+            }
         }
     }
 }
